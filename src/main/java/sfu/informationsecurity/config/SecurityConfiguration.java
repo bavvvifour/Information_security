9f9c2b9bf35d50950c74342ec9fbb999fffa5411
bavvvifour <bayanovv8091@gmail.com>
2025-05-11 11:30:19 +0700

    add https and csrf

--- src/main/java/sfu/informationsecurity/config/SecurityConfiguration.java
+++ src/main/java/sfu/informationsecurity/config/SecurityConfiguration.java
@@ -22,7 +22,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/registration", "/css/**", "/js/**", "/images/**", "/error",
